d3d627c6e87863e629a89c997593abcf7d65cb0d
Abdullah7270 <105360710+Abdullah7270@users.noreply.github.com>
2023-11-22 20:53:49 -0800

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,85 @@
+import openai
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+app = FastAPI()
+
+function_descriptions = [
+    {
+        "name": "extract_info_from_email",
+        "description": "categorise & extract key info from an email, such as use case, company name, contact details, etc.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "companyName": {
+                    "type": "string",
+                    "description": "the name of the company that sent the email"
+                },                                        
+                "product": {
+                    "type": "string",
+                    "description": "Try to identify which product the client is interested in, if any"
+                },
+                "amount":{
+                    "type": "string",
+                    "description": "Try to identify the amount of products the client wants to purchase, if any"
+                },
+                "category": {
+                    "type": "string",
+                    "description": "Try to categorise this email into categories like those: 1. Sales 2. customer support; 3. consulting; 4. partnership; etc."
+                },
+                "nextStep":{
+                    "type": "string",
+                    "description": "What is the suggested next step to move this forward?"
+                },
+                "priority": {
+                    "type": "string",
+                    "description": "Try to give a priority score to this email based on how likely this email will leads to a good business opportunity, from 0 to 10; 10 most important"
+                },
+            },
+            "required": ["companyName", "amount", "product", "priority", "category", "nextStep"]
+        }
+    }
+]
+
+
+# 2 FASTAPI - Turn AI app into webservice
+class Email(BaseModel):
+    from_email: str
+    content: str
+
+
+@app.post("/")
+def analyze_email(email:Email):
+    content = email.content
+    query = f"Please extract key information from this email: {content}"
+
+    messages = [{"role":"user", "content": query}]
+
+    response = openai.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        functions= function_descriptions,
+        function_call="auto"
+    )
+
+    arguments = response.choices[0]["message"]["function_call"]["arguments"]
+    companyName = eval(arguments).get("companyName")
+    product = eval(arguments).get("product")
+    amount = eval(arguments).get("amount")
+    category = eval(arguments).get("category")
+    nextStep = eval(arguments).get("nextStep")
+    priority = eval(arguments).get("priority")
+
+    return{
+        companyName:"companyName",
+        product:"product",
+        amount: "amount",
+        category:"category",
+        nextStep:"nextStep",
+        priority:"priority"
+    }
